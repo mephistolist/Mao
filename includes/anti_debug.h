672ae5cece175b8c588f20d8df45adfc0c2846dc
mephistolist <49227141+mephistolist@users.noreply.github.com>
2025-10-25 00:11:08 +0000

    Update anti_debug.h

--- includes/anti_debug.h
+++ includes/anti_debug.h
@@ -2,8 +2,14 @@
 #define PTRACE_H
 
 void anti_debug(void);
+void die(const char *msg);
+void detect_ld_preload(void);
 void check_tracer_pid(void);
+void detect_timing_anomalies(void);
 void block_ptrace_attaches(void);
 void install_seccomp_ptrace_kill(void);
+void check_environment(void);
+void detect_hardware_breakpoints(void);
+void check_memory_integrity(void);
 
 #endif
